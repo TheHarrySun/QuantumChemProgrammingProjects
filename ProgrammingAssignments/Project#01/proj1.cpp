81622c4e7685511ef4ae3e401bc3defe7723d892
Harrison Xu <hsx925@gmail.com>
2025-01-08 00:00:33 -0800

    working on step 2

--- ProgrammingAssignments/Project#01/proj1.cpp
+++ ProgrammingAssignments/Project#01/proj1.cpp
@@ -1,32 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-vector<vector<double>> readFile(string filename)
+double bondLength(double *atom1, double *atom2)
 {
+    cout << atom2[2] << endl;
+    cout << sizeof(atom1) << " " << sizeof(atom1[0]) << endl;
+    cout << sizeof(*atom1) / sizeof(atom1[0]) << endl;
+    if (sizeof(atom1) / sizeof(atom1[0]) != 3 || sizeof(atom2) / sizeof(atom2[0]) != 3)
+    {
+        cerr << "Atoms must have three coordinates: x, y, and z." << endl;
+        exit(1);
+    }
+    double sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        sum += pow(atom1[i] - atom2[i], 2);
+    }
+    return sqrt(sum);
+}
+
+int main(int argc, char *argv[])
+{
+    cout << argc << endl;
+    if (argc <= 1)
+    {
+        cerr << "File Must Be Inputted" << endl;
+        return 1;
+    }
     ifstream file;
+    string filename = argv[1];
+    cout << filename << endl;
     file.open(filename);
     if (!file.is_open())
     {
-        cerr << "File was not successfully opened.";
-        return;
+        cerr << "File is not successfully opened." << endl;
     }
     int n;
     file >> n;
-    double[] *all_atoms = new double[n][3];
+    double coords[n][3];
+    int zscores[n];
     for (int i = 0; i < n; i++)
     {
-        vector<double> coord;
+        int z;
+        file >> z;
+        zscores[i] = z;
+        cout << zscores[i] << " ";
         for (int j = 0; j < 3; j++)
         {
-            double val;
-            file >> val;
-            coord.push_back(val);
+            double coord;
+            file >> coord;
+            coords[i][j] = coord;
+            cout << coords[i][j] << " ";
         }
-        all_atoms.push_back(coord);
+        cout << endl;
     }
     file.close();
-    return all_atoms;
+    cout << bondLength(coords[0], coords[1]);
 }
